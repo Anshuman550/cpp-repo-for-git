f3ccf89c77355db1246c60cd5d91e061d54a9400
AK Anshuman <anshumanup33@gmail.com>
2024-08-14 00:37:07 +0530

    initial commit

---
+++ first_file.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define fst first
+#define sec second
+#define max3(x,y,z) max(max(x,y),z)
+#define pb push_back
+#define omp map<int, int>
+#define ump unordered_map<int, int>
+#define vec1 vector<int>
+#define vec2 vector<vec1>
+#define vec3 vector<vec2>
+#define que queue<int>
+#define stk stack<int>
+#define pq priority_queue<int>
+#define isort(arr) sort(arr.begin(),arr.end())
+#define dsort(arr) sort(arr.begin(),arr.end(),greater<int>())
+#define pqr priority_queue<int,vi,greater<int> >
+#define pr pair<int, int>
+#define mp make_pair
+#define mod 1000000007
+#define inf 1e18
+#define w(t) int t; cin>>t; while(t--)
+#define take_arr(arr) for(int i=0; i<arr.size(); i++){cin>>arr[i];}
+#define show_arr(arr) for(int i=0; i< arr.size() ; i++){cout<<arr[i]<<' ';} cout<<endl;
+
+
+void calling_function(){
+    cout<<"Hello World";
+    return;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("../input.txt", "r", stdin);
+    //     freopen("../output.txt", "w", stdout);
+    // #endif
+    
+    calling_function();
+
+    return 0;
+}
